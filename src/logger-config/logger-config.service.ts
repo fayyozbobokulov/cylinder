ed53a1d43c8b42555de242f75b176dd96ddab3b2
fayyozbobokulov <b.fayyoz2000@gmail.com>
2023-05-23 11:02:14 +0500

    fix logging

--- src/logger-config/logger-config.service.ts
+++ src/logger-config/logger-config.service.ts
@@ -11,9 +11,6 @@ export class LoggerConfigService {
       this.logger.setContext('LoggerConfigService');
       this.logger.info(`Changing log level to ${level}`);
       this.logger.logger.level = level;
-      const a = this.logger.logger.isLevelEnabled(level);
-      // this.logger.logger.child({ a });
-      // PinoLogger.root.level = level; // change root logger level
     } catch (err) {
       this.logger.error('Failed to change log level', err);
     }
